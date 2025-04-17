fc2ca299848648754e04bb19cf360a62a3f758ea
Nisarg Gandhi <nisarggandhi2929@gmail.com>
2025-04-17 15:54:34 +0530

    Add files via upload

---
+++ p353 pattern 12345.py
@@ -0,0 +1,9 @@
+n=int(input("enter a number=>"))
+for i in range(1,n+1):
+    k=1
+    for j in range(n,i-1,-1):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print(k,end="")
+        k=k+1
+    print("")
